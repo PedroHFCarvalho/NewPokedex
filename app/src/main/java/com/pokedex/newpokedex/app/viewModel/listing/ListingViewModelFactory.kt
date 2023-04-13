d1893dff7786c0f8986c17148985c0eacf62f380
PedroHFCarvalho <ph.carvalho2612@gmail.com>
2023-04-13 17:08:53 -0300

    fix libs

--- app/src/main/java/com/pokedex/newpokedex/app/viewModel/listing/ListingViewModelFactory.kt
+++ app/src/main/java/com/pokedex/newpokedex/app/viewModel/listing/ListingViewModelFactory.kt
@@ -2,12 +2,11 @@ package com.pokedex.newpokedex.app.viewModel.listing
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
 
 
 class ListingViewModelFactory(private val useCase: ListingUseCase) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ListingViewModel::class.java)) {
             return ListingViewModel(useCase) as T
         }
