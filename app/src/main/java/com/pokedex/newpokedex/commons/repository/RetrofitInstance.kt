9adfa016b2e836c2794cf77232a78859400f859f
AzvalSG <giovanni0205.guimaraes@gmail.com>
2023-04-13 21:14:15 -0300

    Att RetrofitInstance.kt e adicionando URL da API.

--- app/src/main/java/com/pokedex/newpokedex/commons/repository/RetrofitInstance.kt
+++ app/src/main/java/com/pokedex/newpokedex/commons/repository/RetrofitInstance.kt
@@ -3,7 +3,7 @@ package com.pokedex.newpokedex.commons.repository
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = ""
+const val BASE_URL = "https://pokeapi.co/docs/v2/"
 
 object RetrofitInstance {
     val retrofit: Retrofit by lazy {
